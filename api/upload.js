ff9bf3dc7506a4de9d5c15e0294eca6702b5b644
Justin <pendo3247@gmail.com>
2019-04-22 10:29:00 -0400

    add code for uploading music add code for uploading movies

--- api/upload.js
+++ api/upload.js
@@ -3,27 +3,10 @@ const express = require('express');
 const multer = require('multer');
 const bencode = require('bencode');
 const crypto = require('crypto');
-const { ulid } = require('ulid');
 const db = require('./../db');
+const validator = require('./validator');
 
 const app = express.Router();
-
-const verificationTypes = {
-  group: {
-    album: {
-      required: ['artist', 'album', 'original_release_year'],
-      allowed: ['artist', 'album', 'original_release_year'],
-      errorMessage: 'Album group is missing information.'
-    }
-  },
-  release: {
-    album: {
-      required: ['release_type', 'format', 'bitrate', 'media'],
-      allowed: ['release_type', 'format', 'bitrate', 'media'],
-      errorMessage: 'Album is missing information.'
-    }
-  }
-};
 
 /**
  * Saves torrent file to disk
@@ -69,7 +52,7 @@ const hashTorrent = (torrentBuffer) => {
  * @param torrent - torrent object
  * @return {Object} torrent object with buffer and hash attached
  */
-const processTorrent = async (torrent) => {
+const processTorrent = (torrent) => {
   const decodedTorrent = bencode.decode(torrent.buffer);
   delete decodedTorrent.announce;
 
@@ -86,137 +69,153 @@ const processTorrent = async (torrent) => {
   return torrent;
 };
 
-/**
- * Verifies an Object
- *
- * Object verified against an element of verificationTypes
- * Removes all extraneous fields on the input Object
- * Throws error if required fields are missing
- *
- * @param input - Object to be verified
- * @param type - Field of the verificationTypes object
- * @return {Promise<Map<torrent>>}
- */
-const verify = async (input, type) => {
-  const { required } = type;
-  const { allowed } = type;
-  const errMsg = type.errorMessage || 'Missing required fields.';
-
-  const verified = new Map();
-  Object.entries(input).forEach((entry) => {
-    const key = entry[0];
-    const value = entry[1];
-
-    if (allowed.includes(key)) {
-      verified.set(key, value);
-    }
-  });
-
-  await Promise.all(
-    required.map(async (field) => {
-      if (!verified.has(field)) {
-        throw new Error(errMsg);
-      }
-    })
-  );
-
-  return verified;
-};
-
-const upTo = (to, startAt = 1) => {
-  const upToArray = [];
-  for (let i = startAt; i <= to; i++) {
-    upToArray.push(`$${i}`);
-  }
-  return upToArray.toString();
-};
-
-/**
- * Gets the group_id that the input torrent should use
- *
- * Either uses the user provided group number or creates a new group if the
- * user provides one, or creates a new group from the user provided data
- *
- * @param {Object} group - group user input
- * @return {Promise<number>} The group number associated with a torrent
- */
-const getGroup = async (group) => {
-  if (typeof group === 'number') {
-    return group;
-  }
-
-  const verifiedGroup = verify(group, verificationTypes.group.album);
-  const keys = verifiedGroup.keys();
-  const values = verifiedGroup.values();
-  const length = verifiedGroup.size;
-
-  const res = await db.query(
-    `insert into groups (${[...keys].toString()}) values (${upTo(
-      length + 1
-    )}) returning id`,
-    [ulid(), ...values]
-  );
-  group = res.rows[0].id;
-  return group;
-};
-
-/**
- * Stores a release to the database
- *
- * Verifies the releaseInfo and adds all of the metadata that is obtained from
- * the other functions
- *
- * @async
- * @param {Object} torrent - slightly modified user inputted torrent
- * @param {number} group - group (user inputted id or new group id)
- * @param {Object} releaseInfo - extra user provided data
- * @return {Promise<Object>} The data associated with a persisted torrent
- */
-const store = async (torrent, group, releaseInfo) => {
-  const verifiedRelease = await verify(
-    releaseInfo,
-    verificationTypes.release.album
-  );
-  // Add more information to the verified user input
-  verifiedRelease.set('group_id', group);
-  verifiedRelease.set('file_size', torrent.totalFileSize);
-  verifiedRelease.set('original_file_name', torrent.originalname);
-  verifiedRelease.set('file_path', torrent.path);
-  verifiedRelease.set('files', JSON.stringify(torrent.fileList));
-
-  const keys = verifiedRelease.keys();
-  const values = verifiedRelease.values();
-  const length = verifiedRelease.size;
-
-  const res = await db.query(
-    `insert into torrents (${[...keys].toString()}) values (${upTo(
-      length + 1
-    )}) returning id, group_id`,
-    [ulid(), ...values]
-  );
-  return {
-    groupId: res.rows[0].group_id,
-    torrentId: res.rows[0].id
-  };
-};
-
 const torrentUpload = upload.fields([{ name: 'torrent', maxCount: 1 }]);
 
 app.post('/upload', torrentUpload, async (req, res) => {
-  const incomingTorrent = req.files.torrent[0];
-  const incomingGroup = JSON.parse(req.body.group);
-  const releaseInfo = JSON.parse(req.body.info);
+  const release = JSON.parse(req.body.release);
+  const client = await db.pool.connect();
 
-  try {
-    const processedTorrent = processTorrent(incomingTorrent);
-    const torrentWithPath = await saveToDisk(processedTorrent);
-    const group = await getGroup(incomingGroup);
-    const persistedTorrent = await store(torrentWithPath, group, releaseInfo);
-    res.status(200).send(persistedTorrent);
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
+  if (release.torrentType === 'music') {
+    const musicReleaseTypes = (await db.getMusicReleaseTypes()).rows;
+    const musicQualities = (await db.getMusicQualities()).rows;
+    try {
+      await validator.validateMusic({
+        release,
+        musicReleaseTypes,
+        musicQualities
+      });
+
+      // valid if doesn't throw
+      await Promise.all(release.artists.map(validator.validateArtist));
+
+      await db.beginTransaction(client);
+
+      let musicId = release.music;
+      // insert music if necessary
+      if (release.music instanceof Object) {
+        // music comes with at least 1 associated artist
+        // insert the artist(s) if they are not ids
+        const artistsToInsert = release.artists.filter((artist) =>
+          Object.prototype.hasOwnProperty.call(artist, 'name')
+        );
+        const artistIds = release.artists.filter((artist) =>
+          Object.prototype.hasOwnProperty.call(artist, 'id')
+        );
+        const insertedArtists = await db.insertArtists(artistsToInsert, client);
+        const artistsToLink = [...artistIds, ...insertedArtists];
+
+        // insert music and assign it to musicId to be used to create the music_release
+        const insertMusicRes = await db.insertMusic(
+          {
+            music: release.music,
+            artistsToLink
+          },
+          client
+        );
+        musicId = insertMusicRes.rows[0].id;
+      }
+
+      const processedTorrent = processTorrent(req.files.torrent[0]);
+
+      // insert torrent
+      const torrentRes = await db.insertTorrent(
+        {
+          fileSize: processedTorrent.totalFileSize,
+          originalFileName: processedTorrent.originalname,
+          filePath: '/',
+          files: processedTorrent.fileList,
+          uploaderId: '01D91F1JSSW4X5DNKSVPD2J3EM'
+        },
+        client
+      );
+
+      const torrentId = torrentRes.rows[0].id;
+
+      // insert music release
+      await db.insertMusicRelease(
+        { musicId, releaseInfo: release.info, torrentId },
+        client
+      );
+
+      await db.commitTransaction(client);
+    } catch (e) {
+      await db.rollbackTransaction(client);
+      console.log(e);
+      return res.status(400).json({
+        error: e
+      });
+    } finally {
+      client.release();
+    }
+  } else if (release.torrentType === 'movie') {
+    try {
+      await validator.validateMovie({
+        release
+      });
+
+      await db.beginTransaction(client);
+
+      let movieId = release.movie;
+      if (release.movie instanceof Object) {
+        const movieRes = await db.insertMovie(
+          {
+            name: release.movie.name,
+            description: release.movie.description,
+            year: release.movie.year
+          },
+          client
+        );
+
+        movieId = movieRes.rows[0].id;
+      }
+
+      const processedTorrent = processTorrent(req.files.torrent[0]);
+
+      // insert torrent
+      const torrentRes = await db.insertTorrent(
+        {
+          fileSize: processedTorrent.totalFileSize,
+          originalFileName: processedTorrent.originalname,
+          filePath: '/',
+          files: processedTorrent.fileList,
+          uploaderId: '01D91F1JSSW4X5DNKSVPD2J3EM'
+        },
+        client
+      );
+
+      const torrentId = torrentRes.rows[0].id;
+
+      await db.insertVideoRelease(
+        {
+          videoId: movieId,
+          quality: release.info.quality,
+          title: release.info.title,
+          description: release.info.description,
+          torrentId
+        },
+        client
+      );
+
+      await db.commitTransaction(client);
+    } catch (e) {
+      await db.rollbackTransaction(client);
+      console.log(e);
+      return res.status(400).json({
+        error: e
+      });
+    } finally {
+      client.release();
+    }
+  } else if (release.torrentType === 'tv') {
+  } else if (release.torrentType === 'anime') {
+  } else if (
+    release.torrentType === 'software' ||
+    release.torrentType === 'video-game'
+  ) {
+  } else {
+    return res.sendStatus(400);
   }
+  res.sendStatus(200);
 });
 
 module.exports = app;
--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
-const Pool = require('pg-pool');
+const { ulid } = require('ulid');
+const { Pool } = require('pg');
 
 const config = {
   user: process.env.DB_USER,
@@ -13,16 +14,179 @@ const pool = new Pool(config);
 
 // pool.query('truncate users');
 
-module.exports.query = (text, values) => {
+const query = (text, values) => {
   return pool.query(text, values);
 };
 
-module.exports.checkSecret = (secret) => {
+const checkSecret = (secret) => {
   return pool.query('select * from users where secret = $1', [secret]);
 };
 
-module.exports.checkTorrent = (hash) => {
+const checkTorrent = (hash) => {
   return pool.query('select * from torrents where hash = $1', [hash]);
 };
 
-module.exports.pg = pool.client;
+const getMusicReleaseTypes = () => {
+  return pool.query('select * from music_release_types');
+};
+
+const getMusicQualities = () => {
+  return pool.query('select * from music_qualities');
+};
+
+const pg = pool.client;
+
+const isUlid = ({ test }) => {
+  if (!(test instanceof String)) {
+    return false;
+  }
+
+  if (test.length !== 26) {
+    return false;
+  }
+
+  return true;
+};
+
+const getMusic = (musicId) => {
+  if (isUlid({ test: musicId })) {
+    return pool.query('select * from music where id = $1', [musicId]);
+  }
+  return Promise.reject(new Error('Supplied value is not a valid ID.'));
+};
+
+const insertArtists = async (artists, client) => {
+  const clientOrPool = typeof client === 'undefined' ? pool : client;
+  const ids = artists.map(() => ulid());
+  const names = artists.map((artist) => artist.name);
+  const descriptions = artists.map((artist) => artist.description);
+
+  try {
+    await clientOrPool.query(
+      'insert into artists (id, name, description) select * from unnest ($1::text[], $2::text[], $3::text[]) returning id',
+      [ids, names, descriptions]
+    );
+
+    return artists.map((artist, index) => {
+      return {
+        id: ids[index],
+        primary: artist.primary
+      };
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const insertMusic = async ({ music, artistsToLink }, client) => {
+  const clientOrPool = typeof client === 'undefined' ? pool : client;
+  const musicId = ulid();
+  const musicRes = await clientOrPool.query(
+    'insert into music (id, title, description, year, music_release_id) values ($1, $2, $3, $4, $5) returning id',
+    [
+      musicId,
+      music.title,
+      music.description,
+      music.year,
+      music.musicReleaseType
+    ]
+  );
+
+  // link linked artists
+  const artistIds = artistsToLink.map((artist) => artist.id);
+  const musicIds = artistsToLink.map(() => musicId);
+  const artistPrimaries = artistsToLink.map((artist) => artist.primary);
+  await clientOrPool.query(
+    'insert into music_artists (music_id, artist_id, is_primary) select * from unnest ($1::text[], $2::text[], $3::boolean[])',
+    [musicIds, artistIds, artistPrimaries]
+  );
+  return musicRes;
+};
+
+const insertMusicRelease = ({ musicId, torrentId, releaseInfo }, client) => {
+  const clientOrPool = typeof client === 'undefined' ? pool : client;
+  const musicReleaseId = ulid();
+  clientOrPool.query(
+    'insert into music_releases (id, encoding, quality, music_id, torrent_id, title, description) values ($1, $2, $3, $4, $5, $6, $7)',
+    [
+      musicReleaseId,
+      releaseInfo.encoding,
+      releaseInfo.quality,
+      musicId,
+      torrentId,
+      releaseInfo.title,
+      releaseInfo.description
+    ]
+  );
+};
+
+const insertTorrent = (
+  { fileSize, originalFileName, filePath, files, uploaderId },
+  client
+) => {
+  const clientOrPool = typeof client === 'undefined' ? pool : client;
+  return clientOrPool.query(
+    'insert into torrents (id, file_size, original_file_name, file_path, files, uploader_id) values ($1, $2, $3, $4, $5, $6) returning id',
+    [
+      ulid(),
+      fileSize,
+      originalFileName,
+      filePath,
+      JSON.stringify(files),
+      uploaderId
+    ]
+  );
+};
+
+const getMovie = (movieId) => {
+  if (isUlid({ test: movieId })) {
+    return pool.query('select * from movies where id = $1', [movieId]);
+  }
+  return Promise.reject(new Error('Supplied value is not a valid ID.'));
+};
+
+const insertMovie = ({ name, description, year }, client) => {
+  const clientOrPool = typeof client === 'undefined' ? pool : client;
+  return clientOrPool.query(
+    'insert into movies (id, name, description, year) values ($1, $2, $3, $4) returning id',
+    [ulid(), name, description, year]
+  );
+};
+
+const insertVideoRelease = (
+  { videoId, quality, title, description, torrentId },
+  client
+) => {
+  const clientOrPool = typeof client === 'undefined' ? pool : client;
+  return clientOrPool.query(
+    'insert into video_releases (id, video_id, quality, title, description, torrent_id) values ($1, $2, $3, $4, $5, $6) returning id',
+    [ulid(), videoId, quality, title, description, torrentId]
+  );
+};
+
+const beginTransaction = (client) => client.query('begin');
+
+const commitTransaction = (client) => client.query('commit');
+
+const rollbackTransaction = (client) => client.query('rollback');
+
+module.exports = {
+  beginTransaction,
+  commitTransaction,
+  rollbackTransaction,
+  query,
+  checkSecret,
+  checkTorrent,
+  getMusicQualities,
+  getMusicReleaseTypes,
+  pg,
+  getMusic,
+  insertArtists,
+  insertMusic,
+  insertMusicRelease,
+  insertTorrent,
+  getMovie,
+  insertMovie,
+  insertVideoRelease,
+  pool
+};
